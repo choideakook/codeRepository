323e01e02997588de99deb416282bd04f47ccc07
choideakook <shdrnrhd113@naver.com>
2023-02-07 22:32:28 +0900

    230207

---
+++ practice/hard/src/main/java/task/hard/HardApplication.java
@@ -0,0 +1,13 @@
+package task.hard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardApplication.class, args);
+	}
+
+}
