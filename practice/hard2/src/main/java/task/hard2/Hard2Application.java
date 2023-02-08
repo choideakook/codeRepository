8c9340fde273f242393b4294a9cc2fbc1b52b5c3
choideakook <shdrnrhd113@naver.com>
2023-02-08 10:57:51 +0900

    230208

---
+++ practice/hard2/src/main/java/task/hard2/Hard2Application.java
@@ -0,0 +1,13 @@
+package task.hard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hard2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hard2Application.class, args);
+	}
+
+}
